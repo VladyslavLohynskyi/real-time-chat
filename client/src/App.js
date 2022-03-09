0a09b4da665cedd2ff54a645ea51fa4e428cf4ad
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-03-09 17:49:46 +0200

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import EventSourcing from "./EventSourcing";
+import LongPulling from "./LongPulling";
+import WebSock from "./WebSocket";
+
+function App() {
+  return (
+    <div className="App">
+      <WebSock />
+    </div>
+  );
+}
+
+export default App;
